3d6a219fc6d80e566f7d6b831c331d0ac48a4547
Eminence-Pyro <divinennata@gmail.com>
2026-06-04 10:39:32 +0100

    chore: next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
+  experimental: { serverComponentsExternalPackages: ['@neondatabase/serverless', 'bcryptjs'] },
+};
+
+export default nextConfig;
